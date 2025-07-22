1ec411594f84e93ed06b2c1092ee79ae8c991e99
Rafael Hernandez <ralfhael.ha@gmail.com>
2025-07-22 15:41:24 -0500

    crud mongo and nest

---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,75 @@
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+import { isValidObjectId, Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {
+
+  }
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      this.handlerException(error);
+    }
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  async findOne(term: string) {
+    let pokemon: Pokemon | null = null;
+
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+
+    if (!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({ name: term.toLocaleLowerCase().trim() });
+    }
+
+    if (!pokemon) throw new NotFoundException(`registro no econtrado ${term}`);
+
+    return pokemon;
+  }
+
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    let pokemon = await this.findOne(term);
+
+    if (updatePokemonDto.name)
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
+
+    try {
+      await pokemon.updateOne(updatePokemonDto);
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+    } catch (error) {
+      this.handlerException(error);
+    }
+  }
+
+  async remove(id: string) {
+    const { deletedCount } = await this.pokemonModel.deleteOne({_id: id});
+    if (!deletedCount) throw new BadRequestException(`id not found ${id}`);
+  }
+
+  private handlerException(error: any) {
+    if (error.code == 11000)
+      throw new BadRequestException(`duplicación de registro ${JSON.stringify(error.keyValue)}`);
+    throw new InternalServerErrorException('No se pudo crear el registro - revise logs');
+  }
+}
