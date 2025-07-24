dcfc6c5d0b13c86196b3a7b77cc8b6b51494dee1
Rafael Hernandez <ralfhael.ha@gmail.com>
2025-07-24 12:04:42 -0500

    transform data

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,11 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      transform: true,
       forbidNonWhitelisted: true,
+       transform: true,
+       transformOptions: {
+        enableImplicitConversion: true
+       }
     })
   );
 
